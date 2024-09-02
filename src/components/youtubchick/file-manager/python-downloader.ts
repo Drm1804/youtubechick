31064ce8d2cff477ad08ffe4d224cad8c83ea8e3
Alex Four <>
2024-09-02 19:17:31 +0200

    вынес питоновский загрузчик в отдельную сущность

---
+++ src/components/youtubchick/file-manager/python-downloader.ts
@@ -0,0 +1,37 @@
+import { logger } from '../../../helpers/logger.js';
+import { exec } from 'child_process';
+import { DEFAULT_EXT } from './const.js';
+import { conf } from '../../../../config.js';
+
+const log = logger('Python Downloader');
+
+const getCommand = (internalId: string, url: string) => {
+  return `yt-dlp --embed-metadata -f bestaudio -x --audio-format ${DEFAULT_EXT} -o "${conf.storagePath}${internalId}.%(ext)s" ${url}`;
+};
+
+type Func = (...args: string[]) => void;
+export async function pyDownload(
+  url: string,
+  internalId: string,
+  updateCb: Func,
+): Promise<string | undefined> {
+  log.info('File name ' + internalId);
+  return new Promise((resolve) => {
+    const command = getCommand(internalId, url);
+
+    const process = exec(command, (error) => {
+      if (error) {
+        log.error('downloadFile error');
+        log.error(error);
+        resolve(undefined);
+      }
+
+      resolve(internalId);
+    });
+
+    process.stdout.on('data', function (data) {
+      log.info(data);
+      updateCb(data);
+    });
+  });
+}
--- src/components/youtubchick/utils.ts
+++ src/components/youtubchick/utils.ts
@@ -1,5 +1,5 @@
 import { conf } from '../../../config.js';
-import { DEFAULT_EXT } from './file-manager.js';
+import { DEFAULT_EXT } from './file-manager/const.js';
 
 export const getFileWithPath = (internalId: string, ext = DEFAULT_EXT) =>
   `${conf.storagePath}${internalId}.${ext}`;
